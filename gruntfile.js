45f9c414280cafcf003f7f2eb286aa4079fdce8a
zerospy <admin@admin.com>
2014-07-03 17:52:24 +0800

    apage gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+module.exports = function(grunt) {
+  // 配置Grunt各种模块的参数
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+  });
+  // 从node_modules目录加载模块文件
+  //grunt.loadNpmTasks('grunt-contrib-jade');
+  //grunt.loadNpmTasks('grunt-contrib-less');
+  // 每行registerTask定义一个任务
+  //grunt.registerTask('default', ['jade', 'less']);
+};
