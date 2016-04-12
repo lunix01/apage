9e382e11e9d4a432822f5083144e4f880a00026b
lunix01 <646696730@qq.com>
2016-04-12 13:08:56 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,34 @@
 /*
 * name: apage
-* version: 0.1.1
+* version: 0.1.2
 * author: lunix01
-* Copyright (c) 2015
+* Copyright (c) 2015 - 2016
 */
-var gulp = require('gulp')
-var jade = require('gulp-jade')
-var prettify = require('gulp-prettify');
-var less = require('gulp-less')
-var autoprefixer = require('gulp-autoprefixer')
-var cssmin = require('gulp-minify-css');
-var babel = require("gulp-babel")
-var uglify = require('gulp-uglify')
-var concat = require('gulp-concat')
+const gulp = require('gulp');
+const jade = require('gulp-jade');
+const prettify = require('gulp-prettify');
+const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
+const cssmin = require('gulp-minify-css');
+const babel = require("gulp-babel");
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
 
+const paths = {
+    jade: './p/src/jade/**/*.jade',
+    less: './p/src/less/**/*.less',
+    js: './p/src/es/**/*.js',
+    image: './p/src/images/**/*'
+};
 gulp.task('jade', () => {
-  return gulp.src('./p/src/jade/*.jade')
+  return gulp.src(paths.jade)
     .pipe(jade())
     .pipe(prettify())
     .pipe(gulp.dest('./p/build/html/'))
 });
 gulp.task('less', () => {
-  return gulp.src('./p/src/less/*.less')
+  return gulp.src(paths.less)
     .pipe(less())
     .pipe(concat('style.css'))
     .pipe(cssmin({compatibility: 'ie8'}))
@@ -32,16 +39,22 @@ gulp.task('less', () => {
     .pipe(gulp.dest('./p/build/css/'))
 });
 gulp.task('js', () => {
-  return gulp.src('./p/src/es/*.js')
+  return gulp.src(paths.js)
     .pipe(babel())
     .pipe(concat('app.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./p/build/js'))
 });
-gulp.task('watch', () => {
-  gulp.watch('./p/src/jade/*.jade', ['jade']);
-  gulp.watch('./p/src/less/*.less', ['less']);
-  gulp.watch('./p/src/es/*.js', ['js']);
+gulp.task('image', () => {
+  return gulp.src(paths.image)
+    .pipe(imagemin())
+    .pipe(gulp.dest('./p/build/image/'))
 });
-gulp.task('default', ['jade','less','js','watch']);
-gulp.task('apage', ['jade','less','js','watch']);
+gulp.task('watch', () => {
+  gulp.watch(paths.jade, ['jade']);
+  gulp.watch(paths.less, ['less']);
+  gulp.watch(paths.js, ['js']);
+  gulp.watch(paths.image, ['image']);
+});
+gulp.task('default', ['jade', 'less', 'js', 'image', 'watch']);
+gulp.task('apage', ['jade', 'less', 'js', 'image', 'watch']);
